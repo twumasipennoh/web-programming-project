efb614adeb1c28b6205e4fb4d84c5eca12661b8f
Twumasi Pennoh <twumasi.pennoh@gmail.com>
2021-04-06 18:31:52 -0400

    database connection example

---
+++ database_connection_example/process.php
@@ -0,0 +1,14 @@
+<?php include 'database.php';?>
+
+<?php
+
+// create a variable
+$first_name=$_POST['first_name'];
+$last_name=$_POST['last_name'];
+$job_title=$_POST['job_title'];
+$email=$_POST['email'];
+
+//Execute the query
+$sql = "INSERT INTO HR_Tables.Employee(firstName,lastName,jobTitle,emailAddress) VALUES(?,?,?,?)";
+$stmt=$conn->prepare($sql);
+$stmt->execute([$first_name, $last_name, $job_title, $email]);
