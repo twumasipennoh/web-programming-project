941eaf13bd3d29cb53d14a3a5ccf07757c75feb7
jawukuboateng27 <59456830+jawukuboateng27@users.noreply.github.com>
2021-04-23 17:07:22 -0400

    Add files via upload

---
+++ db_connection/deleteEmp.php
@@ -0,0 +1,45 @@
+<?php
+require_once('database.php');
+
+// create a variable
+$employee_ID = filter_input(INPUT_POST, 'eID',
+                            FILTER_VALIDATE_INT);
+$first_name = filter_input(INPUT_POST, 'fname');
+$last_name = filter_input(INPUT_POST, 'lname');
+
+
+$query1 = 'SELECT * FROM HR_Tables.Employee WHERE employeeID = :employeeID; ';
+
+$statement = $conn->prepare($query1);
+$statement->execute([
+  'employeeID' => $employee_ID,
+]);
+$returned_user = $statement->fetch();
+
+if($returned_user != NULL){
+    $returned_user = true;
+}
+
+$statement->closeCursor();
+
+//Execute the query
+$query2 = 'DELETE FROM HR_Tables.Employee WHERE employeeID = :employeeID; ';
+
+$statement = $conn->prepare($query2);
+$statement->execute([
+  'employeeID' => $employee_ID,
+]);
+
+
+$statement->closeCursor();
+
+if(!$returned_user){
+
+  $error = "Error: Wrong employee name or id ";
+  include('../pages/delete_error.php');
+
+} else{
+     include('..\pages\DeletionConfirmation.php');
+}
+
+?>
