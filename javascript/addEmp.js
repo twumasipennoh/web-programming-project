9dc1115910ddad0dd1dc386dd8e95f3dc6dda848
jawukuboateng27 <59456830+jawukuboateng27@users.noreply.github.com>
2021-04-27 13:31:25 -0400

    Update addEmp.js

--- javascript/addEmp.js
+++ javascript/addEmp.js
@@ -2,39 +2,40 @@ var $ = function(id) {
 	return document.getElementById(id);
 };
 
-var resetForm = function() {
-$("eID").value = "";
-
-$("username").value = "";
-
-$("fname").value = "";
-$("lname").value = "";
-
-$("emailerror").innerHTML = "*";
-$("unerror").innerHTML = "*";
-$("fnerror").innerHTML = "*";
-$("lnerror").innerHTML = "*";
-$("jterror").innerHTML="*";
-
-};
+// var resetForm = function() {
+// // $("eID").value = "";
+//
+// $("username").value = "";
+//
+// $("fname").value = "";
+// $("lname").value = "";
+//
+// // $("emailerror").innerHTML = "*";
+// $("unerror").innerHTML = "*";
+// $("fnerror").innerHTML = "*";
+// $("lnerror").innerHTML = "*";
+// $("jterror").innerHTML="*";
+//
+// };
 
 	function validateValues() {
-	var id = $("eID").value;
+	// var id = $("eID").value;
 
-	var emailVal = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-	var email = $("email").value;
+	// var emailVal = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+	// var email = $("email").value;
 	var sub = true;
 
-	if ($("eID").value === ""){
+	// if ($("eID").value === ""){
+	//
+	// 	$("iderror").innerHTML = "This field is required.";
+	// 	sub = false;
+	// }
+	// else{
+	//
+	// 	$("iderror").innerHTML = "";
+	//
+	// }
 
-		$("iderror").innerHTML = "This field is required.";
-		sub = false;
-	}
-	else{
-
-		$("iderror").innerHTML = "";
-
-	}
 	if ($("username").value === ""){
 
 		$("unerror").innerHTML = "This field is required.";
@@ -84,37 +85,36 @@ $("jterror").innerHTML="*";
 
 
 
-
-
-	if (emailVal.test(email)) {
-
-		$("emerror").innerHTML = "";
-
-	}
-	else {
-
-		$("emailerror").innerHTML = "Please enter a valid e-mail address.";
-		sub = false;
-	}
+	// if (emailVal.test(email)) {
+	//
+	// 	$("emerror").innerHTML = "";
+	//
+	// }
+	// else {
+	//
+	// 	$("emailerror").innerHTML = "Please enter a valid e-mail address.";
+	// 	sub = false;
+	// }
 
 	return sub;
 
 };
 
+
 window.onload = function() {
-$("iderror").style.color = "red";
-$("emailerror").style.color = "red";
+// $("iderror").style.color = "red";
+// $("emailerror").style.color = "red";
 $("unerror").style.color = "red";
 $("fnerror").style.color = "red";
 $("lnerror").style.color = "red";
 $("jterror").style.color= "red";
 
 
-$("iderror").innerHTML = "*";
-$("emailerror").innerHTML = "*";
+// $("iderror").innerHTML = "*";
+// $("emailerror").innerHTML = "*";
 $("unerror").innerHTML = "*";
 $("fnerror").innerHTML = "*";
 $("lnerror").innerHTML = "*";
 $("jterror").innerHTML = "*";
-$("resetform").onclick = resetForm;
+// $("resetform").onclick = resetForm;
 };
