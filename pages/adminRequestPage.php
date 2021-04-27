4d966f3483a552443377791b53585b23c77342f9
rz67749 <rz67749@uga.edu>
2021-04-27 11:28:51 -0400

    IDK

--- pages/adminRequestPage.php
+++ pages/adminRequestPage.php
@@ -30,14 +30,8 @@
       <nav id="nav_menu">
         <ul>
           <li><a href="../pages/admin_home_page.php?employeeID=<?php echo $employeeID ?>">Home</a></li>
-<<<<<<< HEAD
-          <li><a href="../pages/requestPage.php?employeeID=<?php echo $employeeID ?>" class="current">Requests</a></li>
-          <li><a href="../pages/payInfo.php?employeeID=<?php echo $employeeID ?>">Pay Info</a></li>
-		      <li><a href="../pages/employee_directory.php?employeeID=<?php echo $employeeID ?>">Employee Directory</a></li>
-=======
           <li><a href="../pages/adminRequestPage.php?employeeID=<?php echo $employeeID ?>" class="current">Requests</a></li>
 	  <li><a href="../pages/adminEmpDirectory.php?employeeID=<?php echo $employeeID ?>">Employee Directory</a></li>
->>>>>>> 373f52089b95f22833500a6d1cbc61938076be83
           <li><a href="../pages/personal_info_page.php?employeeID=<?php echo $employeeID ?>"><img src="../images/profile_img.png" alt="Profile Image" width="30"></a></li>
           <li><a href="../pages/welcome_page.html">Log out</a></li>
         </ul>
