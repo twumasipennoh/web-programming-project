52c5923002352a5dc8f04abcdd1b1a24fbf6db12
Twumasi Pennoh <twumasi.pennoh@gmail.com>
2021-05-02 13:33:02 -0400

    Update database.php

--- db_connection/database.php
+++ db_connection/database.php
@@ -1,9 +1,9 @@
 <?php
-$host="web-programming-db.cmosf0873dbb.us-east-2.rds.amazonaws.com"; //provide endpoint
-$port=3306;
+$host="127.0.0.1"; //provide endpoint
+$port=3307;
 $socket="";
-$user="cs4300_dev";//provide username
-$password="knowledgeapp12"; //provide password
+$user="root";//provide username
+$password=""; //provide password
 $dbname="";
 
 $conn="";
